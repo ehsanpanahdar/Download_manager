bae7cab6f2b262e945ad3d51030970ff303880d5
Ehsan <ehsan.panahdar@gmail.com>
2023-12-14 21:15:18 +0330

    Fixed URL parsing

--- main.go
+++ main.go
@@ -4,23 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"sync"
 )
 
 func main() {
+	const max_chunk_size  = 10000
 	url := Get_url()
 	total_data_size := Init_download(url)
+	
 }
 
 func Get_url() string {
-	fmt.Print( "Enter the URL:" )
-	var url string
-	_ , err := fmt.Scan(&url)
+	fmt.Print( "Enter the URL: " )
+	var URL string
+	_ , err := fmt.Scan(&URL)
 	if( err != nil ) {
 		log.Fatal(err)
 	}
+
+	_ , err = url.ParseRequestURI(URL)
+	if( err != nil ) {
+		URL = "https://" + URL
+		_ , err = url.ParseRequestURI(URL)
+		if( err != nil ) {
+			log.Fatal(err)
+		}
+	}
 	
-	return url
+	return URL
 }
 
 func Init_download(url string) int64 {
